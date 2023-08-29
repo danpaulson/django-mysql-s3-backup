b6fa711739f8511ba470990039bdb2216999f3fa
Dan Paulson <danpaulson@gmail.com>
2023-08-29 12:46:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Dan Paulson",
     author_email="danpaulson@gmail.com",
     description="Simple backup/restore to s3 for mysql",
-    version='1.0.0',
+    version='1.0.2',
     install_requires=[
         'boto3',
         'prompt_toolkit',
