51d77f3d2ac507daa5707d2c9b1939cf15977635
Dan Paulson <danpaulson@gmail.com>
2023-08-29 10:58:25 -0500

    Add daily flag for backup

--- mysql_s3_backup/management/commands/db-backup.py
+++ mysql_s3_backup/management/commands/db-backup.py
@@ -1,4 +1,5 @@
 import boto3
+import datetime
 import os
 
 from django.core.management.base import BaseCommand
@@ -6,6 +7,13 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--daily',
+            action='store_true',
+            help='Modify the object_name to include the day of the week.',
+        )
+
     def handle(self, *args, **options):
         kwargs = {
             'aws_access_key_id': settings.AWS_ACCESS_KEY_ID,
@@ -14,7 +22,12 @@ class Command(BaseCommand):
         }
 
         bucket_name = settings.AWS_BACKUP_BUCKET
-        object_name = '{}.sql'.format(settings.AWS_BACKUP_BUCKET)
+        if options['daily']:
+            day_of_week = datetime.datetime.now().strftime('%A').lower()
+            object_name = f'{bucket_name}.{day_of_week}.sql'
+        else:
+            object_name = f'{bucket_name}.sql'
+
         db_file_path = '/tmp/db.sql'
 
         s3 = boto3.client('s3', **kwargs)
